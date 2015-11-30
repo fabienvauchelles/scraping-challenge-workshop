6c6fc30524b85d1da03659f5727c51d3e94a6b65
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-30 10:58:02 +0100

    onepage: skeleton

--- myscraper/spiders/myscraper.py
+++ myscraper/spiders/myscraper.py
@@ -11,10 +11,26 @@ class MyScraper(Spider):
     def start_requests(self):
         # First request
         yield Request(
-            url=u'FILL_WITH_URL',
+            url=# TO FILL,
             callback=self.parse,
         )
 
 
     def parse(self, response):
-        pass
+        # Find a list of div which contains a person (use CSS)
+        persons_el = # TO FILL
+
+        # Browse the list
+        for person_el in persons_el:
+
+            # Create a new item
+            item = PersonItem()
+
+            # Extract the name of the person (use CSS)
+            item['name'] = # TO FILL
+
+            # Extract the ticket fare of the person (use CSS)
+            # TO FILL
+
+            # Export the item
+            yield item
