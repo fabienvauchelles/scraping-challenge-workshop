1d1b70aabd4bfc173686b5dac44d1eff1ffeb200
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-30 11:04:49 +0100

    scrapoxy: skeleton

--- myscraper/spiders/myscraper.py
+++ myscraper/spiders/myscraper.py
@@ -9,12 +9,29 @@ class MyScraper(Spider):
 
 
     def start_requests(self):
-        # First request
-        yield Request(
-            url=u'FILL_WITH_URL',
-            callback=self.parse,
-        )
+        # Use a loop (for i in range(0, 1))
+        for i in range(0, 50):
+            yield Request(
+                url='https://scraping-challenge.herokuapp.com/scrapoxy?page=%d' % i,
+                callback=self.parse,
+            )
 
 
     def parse(self, response):
-        pass
+        # Find a list of div which contains a person (use CSS)
+        persons_el = response.css('.person')
+
+        # Browse the list
+        for person_el in persons_el:
+
+            # Create a new item
+            item = PersonItem()
+
+            # Extract the name of the person (use CSS)
+            item['name'] = person_el.css('.name::text').extract_first()
+
+            # Extract the ticket fare of the person (use CSS)
+            item['ticket_fare'] = person_el.css('.ticket_fare::text').extract_first()
+
+            # Export the item
+            yield item
