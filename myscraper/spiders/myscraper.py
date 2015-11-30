7a508b5da6b6871085bf0012d554e7847416a0d2
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-30 11:06:43 +0100

    login: skeleton

--- myscraper/spiders/myscraper.py
+++ myscraper/spiders/myscraper.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from scrapy import Spider, Request
+from scrapy import Spider, Request, FormRequest
 from ..items import PersonItem
 
 
@@ -9,12 +9,32 @@ class MyScraper(Spider):
 
 
     def start_requests(self):
-        # First request
+        # 1/ Use a FormRequest instead of a Request
+        # 2/ Pass email and password with formdata
+        # 3/ Find the good URL to post data !
         yield Request(
-            url=u'FILL_WITH_URL',
+            url=u'https://scraping-challenge.herokuapp.com/login',
             callback=self.parse,
         )
 
 
     def parse(self, response):
-        pass
+        # Find a list of div which contains a person (use CSS)
+        persons_el = response.css('.person')
+
+        # Browse the list
+        for person_el in persons_el:
+
+            # Create a new item
+            item = PersonItem()
+
+            # Extract the name of the person (use CSS)
+            item['name'] = person_el.css('.name::text').extract_first()
+
+            # Extract the ticket fare of the person (use CSS)
+            item['ticket_fare'] = person_el.css('.ticket_fare::text').extract_first()
+
+            # Export the item
+            yield item
+
+
