b8b9793d9cd1aa6c70e7a4dd52b36e4b12bcab16
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-30 10:58:02 +0100

    scrapinghub: template

--- myscraper/spiders/myscraper.py
+++ myscraper/spiders/myscraper.py
@@ -11,10 +11,26 @@ class MyScraper(Spider):
     def start_requests(self):
         # First request
         yield Request(
-            url=u'FILL_WITH_URL',
+            url=u'https://scraping-challenge.herokuapp.com/shub',
             callback=self.parse,
         )
 
 
     def parse(self, response):
-        pass
+        # Find a list of div which contains a person (use CSS)
+        persons_el = response.css('.person')
+
+        # Browse the list
+        for person_el in persons_el:
+
+            # Create a new item
+            item = PersonItem()
+
+            # Extract the name of the person (use CSS)
+            item['name'] = person_el.css('.name::text').extract_first()
+
+            # Extract the ticket fare of the person (use CSS)
+            item['ticket_fare'] = person_el.css('.ticket_fare::text').extract_first()
+
+            # Export the item
+            yield item
