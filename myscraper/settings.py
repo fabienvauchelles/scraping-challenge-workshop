10c0203fbab3fa507ef455a29d1ae613df0f6bc7
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-30 11:04:49 +0100

    scrapoxy-adv: skeleton

--- myscraper/settings.py
+++ myscraper/settings.py
@@ -4,3 +4,24 @@ BOT_NAME = 'myscraper'
 
 SPIDER_MODULES = ['myscraper.spiders']
 NEWSPIDER_MODULE = 'myscraper.spiders'
+
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+RETRY_TIMES = 0
+
+# PROXY
+PROXY = 'http://127.0.0.1:8888/?noconnect'
+
+# SCRAPOXY
+API_SCRAPOXY = 'http://127.0.0.1:8889/api'
+API_SCRAPOXY_PASSWORD = 'CHANGE_THIS_PASSWORD'
+
+# BLACKLISTING
+BLACKLIST_HTTP_STATUS_CODES = # TO FILL
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapoxy.downloadmiddlewares.proxy.ProxyMiddleware': 100,
+    'scrapoxy.downloadmiddlewares.wait.WaitMiddleware': 101,
+    'scrapoxy.downloadmiddlewares.scale.ScaleMiddleware': 102,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': None,
+    # TO FILL (use the BlacklistDownloaderMiddleware from Scrapoxy SDK)
+}
--- myscraper/spiders/myscraper.py
+++ myscraper/spiders/myscraper.py
@@ -9,12 +9,28 @@ class MyScraper(Spider):
 
 
     def start_requests(self):
-        # First request
-        yield Request(
-            url=u'FILL_WITH_URL',
-            callback=self.parse,
-        )
+        for i in range(0, 50):
+            yield Request(
+                url='https://scraping-challenge.herokuapp.com/scrapoxy-adv?page=%d' % i,
+                callback=self.parse,
+            )
 
 
     def parse(self, response):
-        pass
+        # Find a list of div which contains a person (use CSS)
+        persons_el = response.css('.person')
+
+        # Browse the list
+        for person_el in persons_el:
+
+            # Create a new item
+            item = PersonItem()
+
+            # Extract the name of the person (use CSS)
+            item['name'] = person_el.css('.name::text').extract_first()
+
+            # Extract the ticket fare of the person (use CSS)
+            item['ticket_fare'] = person_el.css('.ticket_fare::text').extract_first()
+
+            # Export the item
+            yield item
