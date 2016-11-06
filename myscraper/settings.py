8a864c99579441c2eecfeb409e189f6c44d8f1e3
Fabien Vauchelles <fabien@vauchelles.com>
2016-11-06 15:31:33 +0100

    useragent: soluce

--- myscraper/settings.py
+++ myscraper/settings.py
@@ -6,4 +6,4 @@ SPIDER_MODULES = ['myscraper.spiders']
 NEWSPIDER_MODULE = 'myscraper.spiders'
 
 # Change user agent
-# TO FILL
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
